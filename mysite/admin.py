c86e02807c645733c7a4db58adb4d7813d72ccf1
CHIINGTING <tad20077@gmail.com>
2021-08-23 02:01:55 +0800

    shop done

--- mysite/admin.py
+++ mysite/admin.py
@@ -6,5 +6,17 @@ from mysite import models
 
 admin.site.register(models.Maker)
 admin.site.register(models.PModel)
-admin.site.register(models.Product)
-admin.site.register(models.PPhoto)
+
+
+class pouductAdmin(admin.ModelAdmin):
+    list_display = ('pmodel', 'nickname', 'price', 'year')
+    search_fields = ('nickname', 'description',)
+    ordering = ('-price',)
+
+
+class photoAdmin(admin.ModelAdmin):
+    list_display = ('product', 'description')
+
+
+admin.site.register(models.Product, pouductAdmin)
+admin.site.register(models.PPhoto, photoAdmin)
