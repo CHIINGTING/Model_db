59858bc05d31de374d59e861c95f2b77bc7000d6
CHIINGTING <tad20077@gmail.com>
2021-08-24 01:57:46 +0800

    add mysql

--- Model_db/settings.py
+++ Model_db/settings.py
@@ -74,10 +74,24 @@ WSGI_APPLICATION = 'Model_db.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'model_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '192.168.1.83',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
